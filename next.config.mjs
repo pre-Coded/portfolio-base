ecb2948839b46c0b7b970ff40ceba372a959446d
pre-Coded <princekr.it.21@gmail.com>
2024-03-29 05:52:58 +0530

    next-config-changes

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    eslint: {
+        ignoreDuringBuilds: true,
+    },
+};
 import withVideo from 'next-videos'
 
 export default nextConfig;
